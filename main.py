acd49f25ceeec83cd6e67636647755e6341fda7d
oleg <olegpustovalov220@gmail.com>
2024-03-27 23:19:26 +0300

    toc 3

--- main.py
+++ main.py
@@ -72,20 +72,20 @@ def filter_fgbu_fkp(check_txt):
         return None
 
 
-def get_num_of_str(str_adr):
-    """Извлечь из строки адреса (номер дома, здания)"""
-    length = len(str_adr)
-    integers = []
-    i = 0  # индекс текущего символа
-    while i < length:
-        s_int = ''  # строка для нового числа
-        while i < length and '0' <= str_adr[i] <= '9':
-            s_int += str_adr[i]
-            i += 1
-        i += 1
-        if s_int != '':
-            integers.append(int(s_int))
-    return integers
+# def get_num_of_str(str_adr):
+#     """Извлечь из строки адреса (номер дома, здания)"""
+#     length = len(str_adr)
+#     integers = []
+#     i = 0  # индекс текущего символа
+#     while i < length:
+#         s_int = ''  # строка для нового числа
+#         while i < length and '0' <= str_adr[i] <= '9':
+#             s_int += str_adr[i]
+#             i += 1
+#         i += 1
+#         if s_int != '':
+#             integers.append(int(s_int))
+#     return integers
 
 
 def wrtie_info_in_file_xls_pack(result):
@@ -107,9 +107,9 @@ def wrtie_info_in_file_xls_pack(result):
     sheet.column_dimensions['D'].width = 60
     sheet.column_dimensions['E'].width = 100
     sheet.column_dimensions['F'].width = 16
-
-    for column in range(1, 7):
-        sheet.cell(row=1, column=column).fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+    # # Применение "FFFF00" цвета к столбцам
+    # for column in range(1, 7):
+    #     sheet.cell(row=1, column=column).fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
 
     index = 2
     for key, value in result.items():
@@ -122,15 +122,23 @@ def wrtie_info_in_file_xls_pack(result):
                 address = company_metadata.get('address')  # "Адрес организации"
                 name = property.get('name')  # "Название организации"
                 contact_email = company_metadata.get('url')  # "Контактные данные"
+                # if contact_email == None:
+                #     contact_email = ''
                 contact_phone = company_metadata.get('Phones')
-                phone_numbers_string = ''
-                for phone in contact_phone:
-                    phone_number = phone.get('formatted')
-                    phone_numbers_string += f"{phone_number} | "
-                phone_numbers_info = phone_numbers_string[:-2]  # "Контактные данные"
-                contact_work_time = company_metadata.get('Hours').get('text')  # "Контактные данные"
+                if contact_phone is not None:
+                    phone_numbers_string = ''
+                    for phone in contact_phone:
+                        phone_number = phone.get('formatted')
+                        phone_numbers_string += f"{phone_number} | "
+                    phone_numbers_info = phone_numbers_string[:-2]  # "Контактные данные"
+                else:
+                    phone_numbers_info = ''
+                if company_metadata.get('Hours') is not None:
+                    contact_work_time = company_metadata.get('Hours').get('text')  # "Контактные данные"
+                else:
+                    contact_work_time = ''
                 id_yandex = company_metadata.get('id')  # "ID организации"
-                # Запись данных без фильтрации
+                # Запись данных
                 sheet.cell(row=index, column=1, value=date_today)  # "Дата сверки"
                 sheet.cell(row=index, column=2, value=description)  # "Название"
                 sheet.cell(row=index, column=3, value=address)  # "Адрес организации"
@@ -141,38 +149,13 @@ def wrtie_info_in_file_xls_pack(result):
 
     workbook.save("outpack.xlsx")
 
-#
-# def write_in_folder_and_file_csv(result):
-#     # Создаем папку с датой проверки
-#     today = datetime.now().strftime('%Y-%m-%d')
-#     create_folder(today)
-#     num_dom_list = get_num_of_str(result['input_adr'])
-#     print("Номер дома:", *num_dom_list)
-#
-#     for key, value in result.items():
-#         if type(value) == list:
-#             for v in value:
-#                 property = v.get('properties')
-#                 print(property)
-#                 name_company = filter_fgbu_fkp(property.get('name'))
-#                 if (num_dom_list == get_num_of_str(property.get('description'))) & (name_company != None):
-#                     new_name_file = today + '/' + property.get('name') + '.csv'
-#                     with open(new_name_file, 'w') as csvfile:
-#                         fieldnames = ['Дата проверки', 'Адрес', 'Название компании', 'Контактные данные']
-#                         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#                         writer.writeheader()
-#                         writer.writerow({'Дата сверки': today,
-#                                          'Адрес организации': property.get('description'),
-#                                          'Название организации': property.get('name'),
-#                                          'Контактные данные': property.get('url')})
 
 
 def get_info_api(token, list_adr):
     ''' Получения данных с API Поиска по организациям'''
     for adr in list_adr:
-        time.sleep(1)  # Задержка в 1 секунду
+        # time.sleep(1)  # Задержка в 1 секунду
         url = 'https://search-maps.yandex.ru/v1/?text={}&type=biz&results=50&lang=ru_RU&apikey={}'.format(adr, token)
-        # print(url)
         res = requests.get(url)
         contents = res.text
         result = json.loads(contents)
@@ -187,7 +170,7 @@ def get_info_api(token, list_adr):
 
 
 if __name__ == '__main__':
-    # Проверим наличии исходного файла с адресами
+    # Проверим наличие исходного файла с адресами
     check_file(input_file)
     # Проверим наличия токена в файле
     token = get_token(token_file)
@@ -197,3 +180,4 @@ if __name__ == '__main__':
     get_info_api(token, adr_list)
     # wrtie_info_in_file()
     show_data_now()
+
